06bcee2fd96e9bc2c17f6a9425537f9ef3c8211f
Michael Brzozowski <mikebrzo@gmail.com>
2023-10-14 23:48:12 -0400

    Add more comments

--- transcript_searcher/src/tf_idf_transcript_search.cpp
+++ transcript_searcher/src/tf_idf_transcript_search.cpp
@@ -12,6 +12,7 @@ TfIdfTranscriptSearch::TfIdfTranscriptSearch(const std::string database_path) {
 }
 
 void TfIdfTranscriptSearch::connectDatabase(const std::string database_path) {
+    // Create unique pointer for database object
     db = std::make_unique<SQLite::Database>(database_path);
 }
 
@@ -23,8 +24,9 @@ void TfIdfTranscriptSearch::preprocessTermsCandidates(
     SQLite::Statement count_query(*db, "SELECT COUNT(*) FROM documents");
     // Resolves to true if we had a result from query meaning this term is found in document(s)
     count_query.executeStep();
-    int num_documents_total = count_query.getColumn(0).getInt();
     // Get the number of documents for use in IDF calculation
+    int num_documents_total = count_query.getColumn(0).getInt();
+    // Get the list of documents each term appears in
     for (auto term : search_terms) {
         SQLite::Statement term_query(*db, "SELECT documents FROM terms WHERE term = ?");
         term_query.bind(1, term);
@@ -33,8 +35,12 @@ void TfIdfTranscriptSearch::preprocessTermsCandidates(
             rapidjson::Document documents_json;
             std::string result = term_query.getColumn(0);
             documents_json.Parse(result.c_str());
+            // Get the number of documents this term appears in
             int num_documents_term = documents_json.Size();
             for (int i = 0; i < num_documents_term; i++) {
+                // Add each document to the candidate document set, and initialize its TF-IDF sum score to 0.0
+                // (this may be peformed redundantly if the same document appears for other terms, but effectively
+                // the map will act as a set of documents which appear in union of terms)
                 candidate_documents[documents_json[i].GetString()] = 0.0;
             }
             // Compute and store IDF for this term
@@ -97,15 +103,20 @@ void TfIdfTranscriptSearch::calculateTfIdfScores(
     }
 }
 
-// Custom compare function to change priority queue to behave as min heap
+// Custom compare function to alter a priority queue to behave as min heap
 bool Compare(scored_transcript a, scored_transcript b) {
     return a.second > b.second;
 }
 
+/**
+ * Implements a K-best algorithm by using a K-sized minheap to hold documents by score
+ * The remaining documents in the heap are the K-best in reverse order
+*/
 std::vector<scored_transcript> TfIdfTranscriptSearch::getBestDocuments(
     const std::unordered_map<std::string, double>& candidate_documents_scores,
     const unsigned int k
 ) {
+    // Push each document onto the heap sorting by its score, removing the lowest score from heap once we have > K elements
     std::priority_queue<scored_transcript, std::vector<scored_transcript>, std::function<bool(scored_transcript, scored_transcript)>> minHeap (Compare);
     for (auto d_it = candidate_documents_scores.begin(); d_it != candidate_documents_scores.end(); d_it++) {
         minHeap.push(std::make_pair(d_it->first, d_it->second));
@@ -113,15 +124,18 @@ std::vector<scored_transcript> TfIdfTranscriptSearch::getBestDocuments(
             minHeap.pop();
         }
     }
-    std::vector<scored_transcript> best_candidate_documents_sorted;
+    // Retrieve the K-best documents (sorted worst to best)
+    std::vector<scored_transcript> best_candidate_documents;
     while (!minHeap.empty()) {
-        best_candidate_documents_sorted.push_back(minHeap.top());
+        best_candidate_documents.push_back(minHeap.top());
         minHeap.pop();
     }
-    std::reverse(best_candidate_documents_sorted.begin(), best_candidate_documents_sorted.end());
-    return best_candidate_documents_sorted;
+    // Fix the ordering of our documents to be sorted best to worst
+    std::reverse(best_candidate_documents.begin(), best_candidate_documents.end());
+    return best_candidate_documents;
 
 }
+
 void TfIdfTranscriptSearch::getBestTranscriptMatches(
     const std::vector<std::string>& search_terms,
     const unsigned int k,
@@ -134,6 +148,7 @@ void TfIdfTranscriptSearch::getBestTranscriptMatches(
 
     // Calculate the sum of search terms IDF's for each document
     calculateTfIdfScores(search_terms, search_terms_idfs, candidate_documents_scores);
-        
+
+    // Use the score of each document to pick the K-best documents from the set
     best_matches = getBestDocuments(candidate_documents_scores, k);
 }
